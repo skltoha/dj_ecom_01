f69d88841163eabaef2eef62b33c273421f75f7e
Shakil Islam <shakilislamtoha@gmail.com>
2023-08-19 11:12:57 +0600

    product shorting as per menu selection

--- ecom_main/views.py
+++ ecom_main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import get_list_or_404
 from menu import models as me
 from product import models as pp
 
@@ -10,11 +11,21 @@ def index(request):
     }
     return render(request, 'index.html', context)
 
+
 def product(request, productlink):
-    products = pp.Product.objects.filter(as_menu=productlink)
+    menu_list = []
+    menu_list.append(productlink)
+    sub_list = me.menu.objects.filter(parent_menu_id=productlink)
+    for x in sub_list:
+        menu_list.append(x.id)
+        sub_sub_list= me.menu.objects.filter(parent_menu_id=x.id)
+        for y in sub_sub_list:
+            menu_list.append(y.id)
+    
+    products = pp.Product.objects.filter(as_menu__in=menu_list)
     context = {
         'menu_items': menu_item,
         'product_link': productlink,
-        'products': products
+        'products': products,
     }
     return render(request, 'index.html', context)
